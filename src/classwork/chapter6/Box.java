3bed918f8fa888fc3a6f94579dd08b852e3541ce
Siran Hovsepyan <siran.hovsepyan.92@gmail.com>
2023-09-14 00:23:53 +0300

    Figure painter with classes.

--- src/classwork/chapter6/Box.java
+++ src/classwork/chapter6/Box.java
@@ -3,5 +3,10 @@ package classwork.chapter6;
 public class Box {
     double width;
     double height;
-    double doubt;
+    double depth;
+
+    void volume() {
+        System.out.print("Обьем   ");
+        System.out.print(width * height * depth);
+    }
 }
---
+++ src/homework/siran/FigurePainterTest.java
@@ -0,0 +1,11 @@
+package homework.siran;
+
+public class FigurePainterTest {
+    public static void main(String[] args) {
+        FigurePainter figure = new FigurePainter();
+        figure.figureOne(3,'*');
+        figure.figureOne(4,'*');
+        figure.figureOne(5,'*');
+        figure.figureOne(6,'*');
+    }
+}
